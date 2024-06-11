5f2bec00206143d9afa6d2cb1ed3363d5944c6cc
ATHULBIJU123 <ak.athulbijukumar@gmail.com>
2024-06-11 05:59:20 +0530

    usermmanagement: get,add

--- client/UMS/src/App.jsx
+++ client/UMS/src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Container from './Components/LandingPage/Container'
-
 // import './index.css'
 // import SignIn from './Components/LogInPage/LogIn'
 import LogIn from "./Components/LogInPage/LogIn";
 import Admin from "./Components/Admin/Admin";
 import './Components/AddUser/UserNav'
-// import UserNav from "./Components/AddUser/UserNav";
+import AddUser from "./Components/AddUser/AddUser";
 
 import LandingPage from "./Components/LandingPage/Landing";
+import Getuser from "./Components/GetUser/GetUser";
 
 function App() {
   return (
@@ -17,8 +16,10 @@ function App() {
           <Routes>
             <Route path="/" exact element = {<LandingPage />} />
             <Route path="/login" exact element={<LogIn />} />
+            <Route path="/adduser" exact element={<AddUser />}/>
             <Route path="/admin" exact element={<Admin />}/>
-            
+            <Route path="/getUser" exact element={<Getuser />}/>
+
           </Routes>
 
 
